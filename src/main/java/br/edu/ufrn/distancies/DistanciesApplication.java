f2a62a6e59b78e4ea5c6b57a7f722fe888827666
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-07-06 21:00:37 -0300

    create initial structure of function service with spring

---
+++ src/main/java/br/edu/ufrn/distancies/DistanciesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.distancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/distancies/DistanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.distancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
